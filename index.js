b67374ea1264ac0825cd5eca4faaf2d8f0803307
ferretcode <49322226+FerretCode@users.noreply.github.com>
2021-12-18 12:12:35 -0800

    update exports in index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const client = require("./classes/client");
 const command = require("./classes/command");
 const manager = require("./classes/manager");
+const commandHandler = require("./classes/commandHandler");
+const troll = require("./classes/troll");
 
 module.exports = {
   ...client,
   ...command,
   ...manager,
+  ...commandHandler,
+  ...troll,
 };
